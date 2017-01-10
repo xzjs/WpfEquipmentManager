709e587d3aeb26334d897b51e728c6e6618565a2
xzjs <2310976219@qq.com>
2017-01-10 09:49:37 +0800

    初始化提交

---
+++ WpfEquipmentManager/Equipment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEquipmentManager
+{
+    class Equipment
+    {
+        public int Id { get; set; }
+        public int EquipmentClassId { get; set; }
+        public virtual EquipmentClass EquipmentClass { get; set; }
+    }
+}
---
+++ WpfEquipmentManager/EquipmentClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEquipmentManager
+{
+    class EquipmentClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Num { get; set; }
+        public float Price { get; set; }
+        public string Type { get; set; }
+        public string Detail { get; set; }
+        public string Remark { get; set; }
+        public EquipmentClass()
+        {
+            Equipments = new List<Equipment>();
+        }
+        public virtual ICollection<Equipment> Equipments { get; set; }
+    }
+}
